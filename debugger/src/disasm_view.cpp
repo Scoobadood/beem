fe3bc4535cd9ff0ed0bc3c0075b4fbc45c1e1130
Dave <scoobadood@me.com>
2023-01-06 16:54:55 +1100

    FEAT: First disassembled code on screen

--- debugger/src/disasm_view.cpp
+++ debugger/src/disasm_view.cpp
@@ -9,6 +9,7 @@ DisasmView::DisasmView(QWidget *parent) :
     ui(new Ui::DisasmView)
 {
     ui->setupUi(this);
+    ui->txt_disassembly->setCurrentFont(QFont("courier", 12));
 }
 
 DisasmView::~DisasmView()
@@ -16,9 +17,71 @@ DisasmView::~DisasmView()
     delete ui;
 }
 
+QString format_for_display(const Operation& op) {
+
+  QString addr = QString("%1")
+      .arg(0,4,16, QChar('0'));
+  QString label = QString::fromStdString("");
+  QString opc = QString::fromStdString(op.opcode.name);
+  QString arg;
+  switch( op.opcode.addressing_mode ) {
+    case OpCode::Implied:
+      arg = "";
+      break;
+    case OpCode::Immediate:
+      arg = QString("#$%1").arg(op.data, 2, 16, QChar('0'));
+      break;
+    case OpCode::Absolute:
+      arg = QString("$%1").arg(op.data, 4, 16, QChar('0'));
+      break;
+    case OpCode::IndirectIndexedX:
+      arg = QString("($%1,X)").arg(op.data, 2, 16, QChar('0'));
+      break;
+    case OpCode::IndirectIndexedY:
+      arg = QString("($%1),Y").arg(op.data, 2, 16, QChar('0'));
+      break;
+    case OpCode::Indirect:
+      arg = QString("($%1)").arg(op.data, 4, 16, QChar('0'));
+      break;
+    case OpCode::AbsoluteIndexedX:
+      arg = QString("$%1,X").arg(op.data, 4, 16, QChar('0'));
+      break;
+    case OpCode::AbsoluteIndexedY:
+      arg = QString("$%1,Y").arg(op.data, 4, 16, QChar('0'));
+      break;
+    case OpCode::Accumulator:
+      arg = QString("A");
+      break;
+    case OpCode::ZeroPage:
+      arg = QString("$%1").arg(op.data, 2, 16);
+      break;
+    case OpCode::ZeroPageIndexedY:
+      arg = QString("$%1,Y").arg(op.data, 2, 16);
+      break;
+    case OpCode::ZeroPageIndexedX:
+      arg = QString("$%1,X").arg(op.data, 2, 16);
+      break;
+    case OpCode::Relative:
+      arg = QString("$%1").arg(0 + 2 + (int8_t) op.data, 4, 16, QChar('0'));
+      break;
+  }
+  return QString("%1%2%3%4")
+      .arg( addr, -8, ' ')
+  .arg(label, -12, ' ')
+  .arg( opc, -4, ' ')
+  .arg(arg, -10, ' ');
+}
+
 void DisasmView::set_data(std::shared_ptr<std::vector<uint8_t>> memory) {
   memory_ = memory;
   disassemble_from_ = 0;
   uint8_t err = 0;
+
+  auto dis =
   Disassembler::disassemble_all(*memory_, disassemble_from_, err);
+  ui->txt_disassembly->clear();
+  for( const auto & op : dis) {
+    auto formatted_op = format_for_display(op);
+    ui->txt_disassembly->append(formatted_op);
+  }
 }
