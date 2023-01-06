4beafda4c1c215ca61964ad5149d5c69ccdeb127
Dave <scoobadood@me.com>
2023-01-06 15:20:58 +1100

    REF: Moved opcodes into their own files and added convenience methods

--- Chips/M6502/include/cycle_handler.h
+++ Chips/M6502/include/cycle_handler.h
@@ -13,29 +13,5 @@
 using CycleHandler = std::function<void(M6502 *, uint64_t &)>;
 
 CycleHandler cycle_handler(uint16_t ir);
-struct OpCode {
-  uint8_t bytes;
-  uint8_t cycles;
-  bool page_affected;
-  std::string name;
-  enum AddressingMode {
-    Accumulator,
-    Absolute, AbsoluteIndexedX, AbsoluteIndexedY,
-    Immediate, Implied,
-    Indirect, IndirectIndexedX, IndirectIndexedY,
-    Relative, ZeroPage, ZeroPageIndexedX, ZeroPageIndexedY
-  } addressing_mode;
-  OpCode(uint8_t bytes, uint8_t cycles, bool page_affected, std::string name,
-         AddressingMode addressing_mode) //
-      : bytes{bytes} //
-      , cycles{cycles} //
-      , page_affected{page_affected} //
-      , name{std::move(name)} //
-      , addressing_mode{addressing_mode} //
-  {}
-  std::string to_string() const;
-};
-extern const std::map<uint8_t, OpCode> op_codes;
-
 
 #endif //M6502_INCLUDE_CYCLE_HANDLER_H_
---
+++ debugger/src/disassembler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Dave Durbin on 6/1/2023.
+//
+
+#include "disassembler.h"
