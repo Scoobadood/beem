60d60536733a38a8ecbd7cf3e49e9cdd58e5f07d
Dave <scoobadood@me.com>
2023-01-06 15:48:44 +1100

    FEAT: Added disasm_view UI

--- debugger/src/disasm_view.cpp
+++ debugger/src/disasm_view.cpp
@@ -1,8 +1,24 @@
 #include "disasm_view.h"
+#include "ui_disasm_view.h"
+#include "disassembler.h"
 
 #include <QWidget>
 
-DisasmView::DisasmView(
-    QWidget *parent) //
-    : QWidget{parent} //
-{}
+DisasmView::DisasmView(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::DisasmView)
+{
+    ui->setupUi(this);
+}
+
+DisasmView::~DisasmView()
+{
+    delete ui;
+}
+
+void DisasmView::set_data(std::shared_ptr<std::vector<uint8_t>> memory) {
+  memory_ = memory;
+  disassemble_from_ = 0;
+  uint8_t err = 0;
+  Disassembler::disassemble_all(*memory_, disassemble_from_, err);
+}
