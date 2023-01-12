bc14722008a44fb4182a089f9944a1142a90e52f
Dave <scoobadood@me.com>
2023-01-12 17:39:30 +1100

    FEAT: Writes to ACIA CTL are interpreted and logged in detail

--- hardware/6850_ACIA/include/acia_6850.h
+++ hardware/6850_ACIA/include/acia_6850.h
@@ -16,6 +16,18 @@ class Acia {
   void tick(Bus &bus);
 
  private:
+  void master_reset();
+  void write_ctl(uint8_t data, Bus & bus);
+  void mmio_write(uint16_t addr, Bus &bus);
+
+  /* Control register */
+  uint8_t clk_divisor_{};
+  uint8_t stop_bits_{};
+  uint8_t word_length_{};
+  uint8_t parity_{}; // 0 = none, 1 == odd, 2 == even
+  bool tx_int_enabled_{};
+  bool rx_int_enabled_{};
+  bool rts_default_low_{};
 };
 
 #endif // BEEB_HARDWARE_ACIA_6850_H_
--- hardware/6850_ACIA/src/acia_6850.cpp
+++ hardware/6850_ACIA/src/acia_6850.cpp
@@ -5,17 +5,106 @@
 #include "acia_6850.h"
 
 #include "spdlog/spdlog-inl.h"
-#include "spdlog/sinks/basic_file_sink.h"
 
-Acia::Acia()//
+const uint16_t WO_CTL = 0xfe08;
+
+#define MASTER_RESET(c) ((c & 0x3) == 0x03)
+#define CTR_DIV_SEL(c) (c & 0x3)
+#define WORD_SEL(c) ((c >> 2) & 0x07)
+#define TX_CTL_BITS(c) ((c >> 5) & 0x03)
+#define RX_INT_ENBL(c) ((c & 0x80) == 0x80)
+
+Acia::Acia() //
+    : clk_divisor_{1} //
+    , stop_bits_{2} //
+    , word_length_{7} //
+    , parity_{2} //
+    , tx_int_enabled_{false} //
+    , rx_int_enabled_{false} //
 {}
 
 void mmio_read(uint16_t addr, Bus &bus) {
   spdlog::info("ACIA: Read from 0x{:04x}", addr);
 }
 
-void mmio_write(uint16_t addr, Bus &bus) {
-  spdlog::info("ACIA: Write ({:02x}) to 0x{:04x}", bus.get_data(), addr);
+void Acia::master_reset() {
+  spdlog::info("ACIA: Master Reset");
+}
+
+void Acia::write_ctl(uint8_t data, Bus &bus) {
+  if (MASTER_RESET(data)) {
+    master_reset();
+  } else {
+    clk_divisor_ = CTR_DIV_SEL(data);
+    spdlog::info("ACIA: Set clock divisor to {}", clk_divisor_);
+  }
+
+  switch (WORD_SEL(data)) {
+    case 0:word_length_ = 7;
+      parity_ = 2;
+      stop_bits_ = 2;
+      break;
+    case 1:word_length_ = 7;
+      parity_ = 1;
+      stop_bits_ = 2;
+      break;
+    case 2:word_length_ = 7;
+      parity_ = 2;
+      stop_bits_ = 1;
+      break;
+    case 3:word_length_ = 7;
+      parity_ = 1;
+      stop_bits_ = 1;
+      break;
+    case 4:word_length_ = 8;
+      parity_ = 0;
+      stop_bits_ = 2;
+      break;
+    case 5:word_length_ = 8;
+      parity_ = 0;
+      stop_bits_ = 1;
+      break;
+    case 6:word_length_ = 8;
+      parity_ = 2;
+      stop_bits_ = 1;
+      break;
+    case 7:word_length_ = 8;
+      parity_ = 1;
+      stop_bits_ = 1;
+      break;
+  }
+  switch (TX_CTL_BITS(data)) {
+    case 0:rts_default_low_ = true;
+      tx_int_enabled_ = false;
+      break;
+    case 1:rts_default_low_ = true;
+      tx_int_enabled_ = true;
+      break;
+    case 2:rts_default_low_ = false;
+      tx_int_enabled_ = false;
+    case 3:rts_default_low_ = true;
+      tx_int_enabled_ = false;
+  }
+  rx_int_enabled_ = RX_INT_ENBL(data);
+
+  spdlog::info("      Set Word length: {}, {} parity, {} stop bits",
+               word_length_, parity_ == 0 ? "no" : (parity_ == 1 ? "odd" : "even"), stop_bits_
+  );
+  spdlog::info("      RTS: {}, TX interrupts {}enabled",
+               rts_default_low_ ? "low" : "high",
+               tx_int_enabled_ ? "" : "not "
+  );
+  spdlog::info("      RX interrupts {}enabled", rx_int_enabled_ ? "" : "not ");
+}
+
+void Acia::mmio_write(uint16_t addr, Bus &bus) {
+  auto data = bus.get_data();
+  switch (addr) {
+    case WO_CTL:write_ctl(data, bus);
+      break;
+    default:spdlog::info("ACIA: Write ({:02x}) to 0x{:04x}", bus.get_data(), addr);
+      break;
+  }
 }
 
 void Acia::tick(Bus &bus) {
