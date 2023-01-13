fdbfefb037b2af4359d71d63f9f848d8d32c1c84
Dave <scoobadood@me.com>
2023-01-13 17:00:52 +1100

    FEAT: Added ADC stub

---
+++ hardware/ADC/include/adc.h
@@ -0,0 +1,24 @@
+//
+// Created by Dave Durbin on 13/1/2023.
+//
+
+#ifndef BEEB_HARDWARE_ADC_H_
+#define BEEB_HARDWARE_ADC_H_
+
+#include "bus.h"
+
+#include <cstdint>
+
+class Adc {
+ public:
+  explicit Adc(uint16_t base_address);
+
+  void tick(Bus &bus);
+
+ private:
+  uint16_t base_address_;
+  uint8_t channel_;
+  uint8_t mode_;
+};
+
+#endif // BEEB_HARDWARE_ADC_H_
