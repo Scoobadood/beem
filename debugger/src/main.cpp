efcfd1987dd7eca75ac47f76fe8fa8efb957f9f6
Dave <scoobadood@me.com>
2023-01-24 20:55:11 +1100

    FIX: Honour SPDLOG_LEVEL setting and flush log on exit from debugger

--- debugger/src/main.cpp
+++ debugger/src/main.cpp
@@ -2,11 +2,16 @@
 
 #include "deeb_window.h"
 
+#include <spdlog/cfg/env.h>
+
 #include <QApplication>
 
 int main(int argc, char *argv[]) {
+  spdlog::cfg::load_env_levels();
   QApplication a(argc, argv);
   DeebWindow w;
   w.show();
-  return a.exec();
+  auto x = a.exec();
+  spdlog::shutdown();
+  return x;
 }
