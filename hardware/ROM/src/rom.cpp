bb6d3ceab8b8190b3101b9d2832a42a2d4b8228d
Dave <scoobadood@me.com>
2023-02-02 23:33:43 +1100

    FIX: fixed bug thatr allowed writing to ROM

--- hardware/ROM/src/rom.cpp
+++ hardware/ROM/src/rom.cpp
@@ -4,7 +4,7 @@
 #include <spdlog/spdlog-inl.h>
 
 void check_data_size(uint16_t sz) {
-  if ((uint32_t)(sz & 0xffff)  < 0x10000) return;
+  if ((uint32_t) (sz & 0xffff) < 0x10000) return;
 
   auto msg = fmt::format("ROM: Can't allocate {} bytes as it exceeds 64K addressable space.", sz);
   spdlog::critical(msg);
@@ -12,10 +12,11 @@ void check_data_size(uint16_t sz) {
 }
 
 void check_address_range(uint16_t base_addr, uint16_t sz) {
-  uint32_t max_addr = (((uint32_t) base_addr) & 0xffff ) + (((uint32_t) sz) & 0xffff);
+  uint32_t max_addr = (((uint32_t) base_addr) & 0xffff) + (((uint32_t) sz) & 0xffff);
   if (max_addr <= 0x10000) return;
 
-  auto msg = fmt::format("ROM: Can't allocate {} bytes at_bus {:04x} as it exceeds 64K addressable space.", sz, base_addr);
+  auto msg = fmt::format("ROM: Can't allocate {} bytes at_bus {:04x} as it exceeds 64K addressable space.", sz,
+                         base_addr);
   spdlog::critical(msg);
   throw std::runtime_error(msg);
 }
@@ -43,7 +44,8 @@ Rom::Rom(const std::string &file_name, uint16_t bus_addr) //
     spdlog::critical(msg);
     throw std::runtime_error(msg);
   }
-  memory_ = std::make_shared<std::vector<uint8_t>>((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+  memory_ = std::make_shared<std::vector<uint8_t>>((std::istreambuf_iterator<char>(file)),
+                                                   std::istreambuf_iterator<char>());
   file.close();
   check_data_size(memory_->size());
   check_address_range(bus_addr, memory_->size());
@@ -51,16 +53,14 @@ Rom::Rom(const std::string &file_name, uint16_t bus_addr) //
 
 void Rom::tick(const std::shared_ptr<Bus> &bus) {
   auto addr = bus->get_address();
-  if( addr < bus_address_ || addr >= (bus_address_ + memory_->size())) return;
+  if (addr < bus_address_ || addr >= (bus_address_ + memory_->size())) return;
 
   // Read and write memory
   if (bus->tst_RW()) {
-    auto data = memory_->at(addr-bus_address_);
+    auto data = memory_->at(addr - bus_address_);
     bus->set_data(data);
-  } else {
-    auto data = bus->get_data();
-    memory_->at(addr-bus_address_) = data;
   }
+  // else ... Can't write to ROM
 }
 
 uint8_t Rom::at_bus(uint16_t addr) const {
