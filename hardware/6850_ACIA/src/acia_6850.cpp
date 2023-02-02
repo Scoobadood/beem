e3ee5d42cdd4d41f3fbc17ca6ac32639f4d279e7
Dave <scoobadood@me.com>
2023-02-03 08:09:03 +1100

    FIX: Added dummy read response to try to allow IRQ processing to work

--- hardware/6850_ACIA/src/acia_6850.cpp
+++ hardware/6850_ACIA/src/acia_6850.cpp
@@ -25,6 +25,8 @@ Acia::Acia() //
 
 void mmio_read(uint16_t addr, const std::shared_ptr<Bus>& bus) {
   spdlog::info("ACIA: Read from 0x{:04x}", addr);
+  // FIXME: Temp hack to allow IRQ processing on other devices.
+  if( addr == 0xfe08) bus->set_data(0x80);
 }
 
 void Acia::master_reset() {
